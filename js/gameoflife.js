6b383e7755ad0b0e224029d3ad23ed5a598d06a8
Khaleb O'Brien <ebukauche52@gmail.com>
2021-12-04 02:22:23 +0100

    Complete the 'getLivingNeighbors' Function

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -49,7 +49,9 @@ const getNeighborsOf = ([x, y]) => {
   return [[x - 1, y - 1], [x, y - 1], [x + 1, y - 1], [x - 1, y], [x + 1, y], [x - 1, y + 1], [x, y + 1], [x + 1, y + 1]];
 };
 
-const getLivingNeighbors = (cell, state) => {};
+const getLivingNeighbors = (cell, state) => {
+  return getNeighborsOf(cell).filter((n) => contains.bind(state)(n));
+};
 
 const willBeAlive = (cell, state) => {};
 
