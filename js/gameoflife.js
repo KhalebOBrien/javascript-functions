5d039570dc3428aef2c19c0f0a988e58fbd5e108
Khaleb O'Brien <ebukauche52@gmail.com>
2021-12-04 02:01:13 +0100

    Complete the 'contains' Function

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -8,7 +8,9 @@ function same([x, y], [j, k]) {
 }
 
 // The game state to search for `cell` is passed as the `this` value of the function.
-function contains(cell) {}
+function contains(cell) {
+  return this.some(same.bind(null, cell));
+}
 
 const printCell = (cell, state) => {};
 
